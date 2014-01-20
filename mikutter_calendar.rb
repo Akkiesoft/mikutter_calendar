07c53a4031603975e68dafe77274483ad4e0acb4
Akira Ouchi <akkiesoft@marokun.net>
2014-01-20 13:20:03 +0900

    スケジュール表示の拡充
    ・終了時間を追加
    ・スケジュール一覧表示に昇順ソートを導入した

--- mikutter_calendar.rb
+++ mikutter_calendar.rb
@@ -4,26 +4,29 @@ require 'gcalapi'
 Plugin.create :mikutter_calendar do
 
   class CalendarTreeView < Gtk::TreeView
-    COL_TIME  = 0
-    COL_TITLE = 1
+    COL_STIME = 0
+    COL_ETIME = 1
+    COL_TITLE = 2
 
     def initialize
       super
-      liststore = Gtk::ListStore.new(String, String)
+      liststore = Gtk::ListStore.new(String, String, String)
       set_model(liststore)
       renderer = Gtk::CellRendererText.new
-      col = Gtk::TreeViewColumn.new("時刻", renderer, :text => 0)
+      col = Gtk::TreeViewColumn.new("開始時刻", renderer, :text => COL_STIME)
+      append_column(col)      
+      col = Gtk::TreeViewColumn.new("終了時刻", renderer, :text => COL_ETIME)
       append_column(col)
-      col = Gtk::TreeViewColumn.new("予定", renderer, :text => 1)
+      col = Gtk::TreeViewColumn.new("予定", renderer, :text => COL_TITLE)
       append_column(col)
-
       # Calendar
       load_schedule
     end
 
-    def addline(time, title)
+    def addline(stime, etime, title)
       iter = model.append
-      iter[COL_TIME]  = time
+      iter[COL_STIME]  = stime
+      iter[COL_ETIME]  = etime
       iter[COL_TITLE] = title
     end
 
@@ -47,7 +50,7 @@ Plugin.create :mikutter_calendar do
             @schedule = cal.events(:orderby => "starttime", "max-results" => 100)
           rescue => ee
             Message.new({
-              :message => "再読み込みにに失敗しました\n#{ee}",
+              :message => "再読み込みに失敗しました\n#{ee}",
               :system => true
             })
           end
@@ -63,12 +66,17 @@ Plugin.create :mikutter_calendar do
       if @schedule == nil then
         return
       end
+      todaylist = Array.new()
       @schedule.each do |event|
         date = event.st.localtime.strftime("%Y%m%d")
         if date == selected then
-          time = event.st.strftime("%H:%M")
-          addline(time, event.title)
+          stime = event.st.strftime("%H:%M")
+          etime = event.en.localtime.strftime("%H:%M")
+          todaylist << [stime, etime, event.title]
         end
+      end
+      todaylist.sort{|a,b|a[0]<=>b[0]}.each do |s|
+          addline(s[0], s[1], s[2])
       end
     end
   end
